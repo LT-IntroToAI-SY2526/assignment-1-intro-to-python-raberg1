24fd0796a4789ce72bc6fe7844d761346de63ca3
Robert Berg <rberg@cps.edu>
2025-09-05 10:19:54 -0500

    added asserts for absolute and factorial

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -127,7 +127,11 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
 # this line causes the nested code to be skipped if the file is imported instead of run
 if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
+    assert absolute(4) == 4, "absolute of 4 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
+    assert factorial(5) == 120, "factorial of 5 failed"
+    assert factorial(1) == 1, "factorial of 1 failed"
+    assert factorial(0) == 1, "factorial of 0 failed"
     assert every_other([1, 2, 3, 4, 5]) == [
         1,
         3,
