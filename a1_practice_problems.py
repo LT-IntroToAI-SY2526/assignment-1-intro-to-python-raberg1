b35c2ed55e4465fb938c2199598cc647c9f8eca4
Robert Berg <rberg@cps.edu>
2025-09-10 10:37:29 -0500

    finished sum_list

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -81,8 +81,13 @@ def sum_list(lst: List[int]) -> int:
     Returns:
         the sum of the passed in list
     """
-    raise NotImplementedError("sum_list")
-
+    # This is how the built in function is used
+    # return sum(lst)
+    total = 0
+    for element in lst:
+        total += element
+    # print(total)
+    return total
 
 def mean(lst: List[int]) -> float:
     """Takes a list of numbers, and returns the mean of the numbers.
@@ -144,6 +149,8 @@ if __name__ == "__main__":
     assert every_other([1, 2, 3, 4, 5]) == [1,3,5], "every_other of [1,2,3,4,5] failed"
     assert every_other([12, 23, 35, 48, 53, 63]) == [12,35,53], "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
+    assert sum_list([4, 7, 10, 23, 7]) == 51, "sum_list of [4, 7, 10, 23, 7] failed"
+    # print(sum_list([4, 7, 10, 23, 7]))
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
 
